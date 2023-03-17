eab00524bacc4643ee893ff52ab62e31c0d0b2dc
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2023-03-17 22:01:43 +0100

    ✨ remove development warning.

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,6 @@ import { HomeButton } from './HomeButton'
 export default function Home() {
   return (
     <main>
-      <div className="alert alert-info shadow-lg absolute top-4 left-1/2 -translate-x-1/2 max-w-[50%]">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="stroke-current flex-shrink-0 w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
-          </svg>
-          <span>Under active development!</span>
-        </div>
-      </div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-row">
           <Image
